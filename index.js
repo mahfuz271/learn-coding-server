ec67381b15f92df4deb366af7d3b0655ccf7b636
dev-mahfuz <mrsohag04@gmail.com>
2022-10-25 15:14:52 +0600

    courses route added

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ const app = express()
 const port = 4000
 app.use(cors())
 
+const courses = require("./courses.json")
+
 app.get('/courses', (req, res) => {
-    res.send('Hello World!')
+    res.json(courses)
 })
 
-app.get('/course/:id', function (req, res, next) {
-    res.json({ msg: 'This is CORS-enabled for all origins!' })
-})
 
 app.listen(port, () => {
     console.log(`LearnCoding server app listening on port ${port}`)
