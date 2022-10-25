276dd152a59c20127914e5d759f256f67b936d36
dev-mahfuz <mrsohag04@gmail.com>
2022-10-25 14:57:52 +0600

    initial setup

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+var cors = require('cors')
+const app = express()
+const port = 4000
+app.use(cors())
+
+app.get('/courses', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/course/:id', function (req, res, next) {
+    res.json({ msg: 'This is CORS-enabled for all origins!' })
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
